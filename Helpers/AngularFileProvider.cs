d0f5716a782e79bb8ef29c0cfedcd6cfbb86cf70
Benjamin Pannell <admin@sierrasoftworks.com>
2015-02-20 15:57:13 +0200

    Initial commit of project files

---
+++ Helpers/AngularFileProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Builder;
+
+namespace AngularASPNext
+{
+    public static class AngularFileProviderExtensions
+    {
+        public static IApplicationBuilder UseAngularFileServer(this IApplicationBuilder builder)
+        {
+            return builder.UseAngularFileServer("index.html");
+        }
+
+        public static IApplicationBuilder UseAngularFileServer(this IApplicationBuilder builder, params string[] indexPaths)
+        {
+            return builder.Use(next => new AngularFileProviderMiddleware(next, indexPaths).Invoke);
+        }
+    }
+}
---
+++ Middleware/AngularFileProvider.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.StaticFiles;
+using System.Collections.Generic;
+
+namespace AngularASPNext
+{
+    public class AngularFileProviderMiddleware
+    {
+        protected readonly RequestDelegate Next;
+        protected readonly IEnumerable<string> IndexFiles;
+
+        public AngularFileProviderMiddleware(RequestDelegate next, IEnumerable<string> indexFiles)
+        {
+            Next = next;
+            IndexFiles = indexFiles;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if(!context.Request.Method.Equals("GET", StringComparison.InvariantCultureIgnoreCase))
+            {
+                await Next(context);
+                return;
+            }
+
+            if(context.Request.Path.ToString().StartsWith("/api"))
+            {
+                await Next(context);
+                return;
+            }
+
+            context.Response.ContentType = "text/html";
+            foreach(var indexFile in IndexFiles)
+            {
+                try
+                {
+                    await context.Response.SendFileAsync(indexFile);
+                    return;
+                }
+                catch
+                { }
+            }
+
+            context.Response.StatusCode = 404;
+            await context.Response.WriteAsync("<html><body><h1>Not Found</h1><p>Could not find the Angular application entrypoint</p></body></html>");
+        }
+    }
+}
